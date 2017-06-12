23514bdb115e597eed56e0fcb99ee609c6e10586
Marisol Lopez <lmarisolopez@gmail.com>
2017-06-12 13:44:54 -0700

    added api url and function to petlist

--- src/app/collections/pet_list.js
+++ src/app/collections/pet_list.js
@@ -3,7 +3,11 @@ import Backbone from 'backbone';
 import Pet from 'app/models/pet';
 
 var PetList = Backbone.Collection.extend({
-  model: Pet
+  model: Pet,
+  url: 'http://petdibs.herokuapp.com/pets',
+  parse: function(data) {
+    return data.pets;
+  }
 });
 
 export default PetList;
